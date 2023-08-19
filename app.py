f0da52a9ec22c40fc2707bd2894832ecbf3317ef
caiogmello <caiogomedemello@gmail.com>
2023-08-19 17:24:52 -0300

    testing some graphs

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from dash import Dash, html
+from dash import Dash, html, dash_table, dcc
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('./data/station_salvador.csv')
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    html.Div(children='Teste com Salvador'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.line(df, x='YEAR', y='SEP'))
 ])
 
 if __name__ == '__main__':
